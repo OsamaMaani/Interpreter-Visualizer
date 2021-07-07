bdcf8e56ca94b0b03eb7ff6720632593772a220b
Muhammad Al-Halaby <moj1323@gmail.com>
2021-07-07 04:41:55 +0200

    Delete some files

--- InterpreterVisualizer/src/main/java/api/interpretervisualizer/controller/WebController.java
+++ InterpreterVisualizer/src/main/java/api/interpretervisualizer/controller/WebController.java
@@ -1,7 +1,5 @@
 package api.interpretervisualizer.controller;
 
-import api.interpretervisualizer.models.SampleResponse;
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
